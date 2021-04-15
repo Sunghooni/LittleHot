7ef68ae8eae0092bf7967405ee121d8a311abd14
SungHoon <leesunghoon03@naver.com>
2021-04-15 17:51:00 +0900

    조준점 적용
    -발사 시 조준한 방향으로 이동
    -총을 던질 경우에도 적용
    -근접 공격 순서 적용

--- Assets/Scripts/Mouse/MouseCtrl.cs
+++ Assets/Scripts/Mouse/MouseCtrl.cs
@@ -4,28 +4,9 @@ using UnityEngine;
 
 public class MouseCtrl : MonoBehaviour
 {
-    private Camera mainCamera;
-
     private void Awake()
     {
-        mainCamera = gameObject.GetComponent<Camera>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    public GameObject ShotRay()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
-        {
-            if (hit.transform.TryGetComponent(out Gun gun))
-            {
-                gun.HoldedToHand();
-                return hit.transform.gameObject;
-            }
-        }
-
-        return null;
     }
 }
